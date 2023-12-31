4cb25b635745638543173b9462397ed7990e7960
SEGR02 <sebastian.gomz02@gmail.com>
2023-12-31 00:16:01 -0400

    Sebas: delete company endpoint ready || changes on logic about getCompanies by owner endpoint

--- src/controllers/companies.controllers.js
+++ src/controllers/companies.controllers.js
@@ -1,63 +1,82 @@
-
 const CompaniesServices = require("../services/companies.service");
 
 const getCompanyUserOwn = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req.params;
     const result = await CompaniesServices.getOwnedService(userId);
     if (result) {
       res.status(200).json(result);
-    } else if (!result){
-      res.status(400).json({ message: "Not companies found. Controller response" });
+    } else if (!result) {
+      res
+        .status(400)
+        .json({ message: "Not companies found. Controller response" });
     }
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
   }
-}
+};
 
 const createUserCompany = async (req, res) => {
-  try{
-    const data = req.body
+  try {
+    const data = req.body;
     const result = await CompaniesServices.createCompanyService(data);
-    if (result){
-      res.status(201).json(result);  
-    }else if(!result){
+    if (result) {
+      res.status(201).json(result);
+    } else if (!result) {
       res.status(400).json({ message: "Not created. Controller response" });
     }
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
   }
-}
+};
 
 const getCompanyData = async (req, res) => {
-  try{
-    const { companyId } = req.params
+  try {
+    const { companyId } = req.params;
     const result = await CompaniesServices.getCompanyDataService(companyId);
-    if (result){
-      res.status(200).json(result);  
-    }else if(!result){
+    if (result) {
+      res.status(200).json(result);
+    } else if (!result) {
       res.status(400).json({ message: "Company not found" });
     }
-
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
   }
-}
+};
 
 const upadateCompanyData = async (req, res) => {
-  try{
-    const { companyId } = req.params
-    const { 
-      description, creationDate, turnover, employees, location, 
-      companyWebsite, companyOffices, companyCategories, companySubcategories, productName, productVersion
+  try {
+    const { companyId } = req.params;
+    const {
+      description,
+      creationDate,
+      turnover,
+      employees,
+      location,
+      companyWebsite,
+      companyOffices,
+      companyCategories,
+      companySubcategories,
+      productName,
+      productVersion,
     } = req.body;
 
     // Validate mandatory fields
-    if (!description || !creationDate || !turnover || !employees || !location || !companyWebsite || !companyOffices || !companyCategories || !companySubcategories) {
-      return res.status(400).send('Missing mandatory fields');
+    if (
+      !description ||
+      !creationDate ||
+      !turnover ||
+      !employees ||
+      !location ||
+      !companyWebsite ||
+      !companyOffices ||
+      !companyCategories ||
+      !companySubcategories
+    ) {
+      return res.status(400).send("Missing mandatory fields");
     }
 
     // Prepare the data object with only provided fields
@@ -68,34 +87,50 @@ const upadateCompanyData = async (req, res) => {
       employees,
       location,
       companyWebsite,
-      companyOffices, 
-      companyCategories, 
+      companyOffices,
+      companyCategories,
       companySubcategories,
-      ...(productName && { productName }),  // Include only if provided
-      ...(productVersion && { productVersion }),  // Include only if provided
+      ...(productName && { productName }), // Include only if provided
+      ...(productVersion && { productVersion }), // Include only if provided
     };
 
-    const result = await CompaniesServices.updateCompanyDataService(companyId, updatedData);
+    const result = await CompaniesServices.updateCompanyDataService(
+      companyId,
+      updatedData
+    );
 
-    if (result){
+    if (result) {
       //console.log('controllador if entro a true');
-      res.status(200).json({ message: "Company updated" });  
-    }else if(!result){
+      res.status(200).json({ message: "Company updated" });
+    } else if (!result) {
       //console.log('controllado if entro a false');
       res.status(400).json({ message: "Company not found" });
     }
-
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
   }
-}
+};
 
-
+const deleteCompany = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+    const result = await CompaniesServices.deleteCompany(companyId);
+    if (result) {
+      res.status(200).json({ message: "Company deleted" });
+    } else if (!result) {
+      res.status(400).json({ message: "Company not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
+};
 
 module.exports = {
   getCompanyUserOwn,
   createUserCompany,
   getCompanyData,
-  upadateCompanyData
-}
+  upadateCompanyData,
+  deleteCompany,
+};
--- src/routes/companies.router.js
+++ src/routes/companies.router.js
@@ -1,18 +1,17 @@
 const { Router } = require("express");
 const router = Router();
-const { 
-    getCompanyUserOwn, 
-    createUserCompany, 
-    getCompanyData, 
-    upadateCompanyData 
+const {
+  getCompanyUserOwn,
+  createUserCompany,
+  getCompanyData,
+  upadateCompanyData,
+  deleteCompany,
 } = require("../controllers/companies.controllers");
 
-
-
-router.post("/create", createUserCompany);
 router.get("/:companyId", getCompanyData);
-router.post("/userown", getCompanyUserOwn);
-router.put("/:companyId", upadateCompanyData)
-
+router.get("/getByOwner/:userId", getCompanyUserOwn);
+router.post("/create", createUserCompany);
+router.put("/:companyId", upadateCompanyData);
+router.delete("/:companyId", deleteCompany);
 
 module.exports = router;
