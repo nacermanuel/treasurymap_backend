a796bb85333b01f8a121dfdeb9c3c8289cd7014f
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-02 18:23:40 -0400

    Sebas/50% endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const companiesRoutes = require("./companies.router");
 const videosRoutes = require("./videos.router");
 const articlesRoutes = require("./articles.router");
 const answersRoutes = require("./answers.router");
-
+const categoriesRoutes = require("./categories.router");
 const error = require("../middlewares/error.middleware");
 
 const routerApi = (app) => {
@@ -12,6 +12,7 @@ const routerApi = (app) => {
   app.use("/api/v1/videos", videosRoutes, error);
   app.use("/api/v1/articles", articlesRoutes, error);
   app.use("/api/v1/answers", answersRoutes, error);
+  app.use("/api/v1/categories", categoriesRoutes, error);
 };
 
 module.exports = routerApi;
