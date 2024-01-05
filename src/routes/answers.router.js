bed66bdd057464e7a215fc50f541ee2501115163
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-04 21:10:27 -0400

    Sebas/Some bug fixes solved on answers controller and service

--- src/routes/answers.router.js
+++ src/routes/answers.router.js
@@ -1,17 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 const {
-    CreateAnswer,
-    GetAnswerByCompanyId,
-    updateAnswer,
+  CreateAnswer,
+  GetAnswerByCompanyId,
+  updateAnswer,
 } = require("../controllers/answers.controllers");
 
-
-router.post("/create", CreateAnswer );
-router.put("/:answerId", updateAnswer)
-router.get("/:companyId", GetAnswerByCompanyId)
-
-/*INSERT*/
-
+router.get("/:companyId", GetAnswerByCompanyId);
+router.post("/:companyId", CreateAnswer);
+router.put("/:answerId", updateAnswer);
 
 module.exports = router;
