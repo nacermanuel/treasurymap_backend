9d475390ddab950085ec7baa144f0b780d7dd523
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-06 08:40:05 -0400

    Sebas: Some changes on answers reception data logic

--- src/controllers/countries.controllers.js
+++ src/controllers/countries.controllers.js
@@ -9,6 +9,17 @@ const getAllCountries = async (req, res, next) => {
   }
 };
 
+const getCountryById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await CountriesServices.getCountryById(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllCountries,
+  getCountryById,
 };
