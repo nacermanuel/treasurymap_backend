890cbb6c2c27af681fdffd90df17ef381bb40fee
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-12 01:44:59 -0400

    Sebas: Delete all answers by company id endpoint

--- src/routes/answers.router.js
+++ src/routes/answers.router.js
@@ -4,10 +4,12 @@ const {
   CreateAnswer,
   GetAnswerByCompanyId,
   updateAnswer,
+  deleteAllAnswersByCompanyId,
 } = require("../controllers/answers.controllers");
 
 router.get("/:companyId", GetAnswerByCompanyId);
 router.post("/:companyId", CreateAnswer);
 router.put("/:answerId", updateAnswer);
+router.delete("/:companyId", deleteAllAnswersByCompanyId);
 
 module.exports = router;
