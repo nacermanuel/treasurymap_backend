39aa770799043ac5a2818c52dbebe1121693bc81
SEGR02 <sebastian.gomz02@gmail.com>
2024-01-16 01:50:28 -0400

    Sebas: Get user by id and update user endpoints

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,6 +9,29 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await UsersServices.getUserById(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const result = await UsersServices.updateUserById(id, data);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getAllUsers,
+  getUserById,
+  updateUserById,
 };
